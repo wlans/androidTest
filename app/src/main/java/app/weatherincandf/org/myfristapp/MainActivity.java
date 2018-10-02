d12e25fe370dafbe28dd88008a2ad65e7408e673
wlans <wyatt.lansdale@gmail.com>
2018-10-01 20:08:52 -0500

    Finsihes layout and starts java code

--- app/src/main/java/app/weatherincandf/org/myfristapp/MainActivity.java
+++ app/src/main/java/app/weatherincandf/org/myfristapp/MainActivity.java
@@ -2,6 +2,7 @@ package app.weatherincandf.org.myfristapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
     }
 }
+
+// https://www.youtube.com/watch?v=dFlPARW5IX8&t=869s
